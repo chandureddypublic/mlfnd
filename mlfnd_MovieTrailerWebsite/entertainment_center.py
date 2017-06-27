de862c4b77f4be5736031dcc79626ecad9196c41
chandureddypublic <CHANDUREDDYK.PUBLIC@GMAIL.COM>
2017-06-27 13:03:25 +0530

    Add files via upload
    Movie Trailer Website - Initial version.

---
+++ mlfnd_MovieTrailerWebsite/entertainment_center.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""     entertainment_center: creates Movie instances and associates Movies to
+        Movie Database.
+        Then Opens the Movie Webpage to display Movie Trailers to allow user
+        to play selected trailer.
+"""
+__author__ = "Chandra SRK "
+__copyright__ = "Copyright 2017, The Project MLFND-PF"
+__credits__ = [" ", " "]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Chandra SRK"
+__email__ = ""
+__status__ = "Alpha"
+
+
+# standard library imports
+
+# third party imports for movie webpage
+import fresh_tomatoes
+
+# local application/library specific imports
+import media
+
+toy_story = media.Movie(
+            "Toy Story",
+            "A Story of a boy and his toys that come to life",
+            "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+            "https://www.youtube.com/watch?v=vwyZH85NQC4",
+            media.Movie.VALID_RATINGS[0]
+                )
+avatar = media.Movie(
+            "Avatar",
+            "A marine on an alien planet",
+            "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+            "http://www.youtube.com/watch?v=-9ceBgWV8io",
+            media.Movie.VALID_RATINGS[2]
+                )
+dj = media.Movie(
+            "DJ",
+            "Gudilo Badilo Madilo Vodilo Full Song",
+            "https://naasongs.com/wp-content/uploads/2017/06/DJ-Duvvada-Jagannadham-2017.jpg",
+            "https://www.youtube.com/watch?v=ul-YyTYvIRE&spfreload=5",
+            media.Movie.VALID_RATINGS[0]
+            )
+
+movieDbList = [toy_story, avatar, dj]
+print("Movie Server info:")
+print(" "*4 + "Number of Movies:" + str(len(movieDbList)))
+
+fresh_tomatoes.open_movies_page(movieDbList)
+print("Movie Server Started .")
+
+
+# end of entertainment_center
---
+++ mlfnd_MovieTrailerWebsite/media.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""       media:
+            Creates Movie Class to hold following movie specific information:
+                        1.title, 2. storyline, 3.poster_image_url,
+                        4.trailer_youtube_url, 5.rating
+            implements show_trailer() method
+"""
+__author__ = "Chandra SRK "
+__copyright__ = "Copyright 2017,The Project MLFND-PF-mlfnd_MovieTrailerWebsite"
+__credits__ = [" ", " "]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Chandra SRK"
+__email__ = ""
+__status__ = "Alpha"
+
+# standard library imports
+import webbrowser
+
+# third party imports for movie webpage
+#  n/a
+# local application/library specific imports
+#  n/a
+
+
+#  Class Defition for Movie
+class Movie:
+    """This class provides a way to store movie related information """
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+# method - constructor
+    def __init__(
+                self,
+                movie_title,
+                movie_storyline,
+                movie_poster_image_url,
+                movie_trailer_youtube_url,
+                rating):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_image_url
+        self.trailer_youtube_url = movie_trailer_youtube_url
+        self.rating = rating
+
+# method - show_trailer
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+# end of Media
