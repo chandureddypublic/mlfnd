1fe3347418e5e93435c162d1083c661f591bbe9e
chandureddypublic <CHANDUREDDYK.PUBLIC@GMAIL.COM>
2017-06-27 19:50:53 +0530

    Add files via upload
    Review comments incorporated: corrected coding standard specific errors thrown by PEP8

--- mlfnd_MovieTrailerWebsite/entertainment_center.py
+++ mlfnd_MovieTrailerWebsite/entertainment_center.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# standard library imports
+
+# third party imports for movie webpage
+import fresh_tomatoes
+
+# local application/library specific imports
+import media
+
 """     entertainment_center: creates Movie instances and associates Movies to
         Movie Database.
         Then Opens the Movie Webpage to display Movie Trailers to allow user
@@ -14,33 +22,24 @@ __maintainer__ = "Chandra SRK"
 __email__ = ""
 __status__ = "Alpha"
 
-
-# standard library imports
-
-# third party imports for movie webpage
-import fresh_tomatoes
-
-# local application/library specific imports
-import media
-
 toy_story = media.Movie(
             "Toy Story",
             "A Story of a boy and his toys that come to life",
-            "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+            "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # noqa
             "https://www.youtube.com/watch?v=vwyZH85NQC4",
             media.Movie.VALID_RATINGS[0]
                 )
 avatar = media.Movie(
             "Avatar",
             "A marine on an alien planet",
-            "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+            "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # noqa
             "http://www.youtube.com/watch?v=-9ceBgWV8io",
             media.Movie.VALID_RATINGS[2]
                 )
 dj = media.Movie(
             "DJ",
             "Gudilo Badilo Madilo Vodilo Full Song",
-            "https://naasongs.com/wp-content/uploads/2017/06/DJ-Duvvada-Jagannadham-2017.jpg",
+            "https://naasongs.com/wp-content/uploads/2017/06/DJ-Duvvada-Jagannadham-2017.jpg",  # noqa
             "https://www.youtube.com/watch?v=ul-YyTYvIRE&spfreload=5",
             media.Movie.VALID_RATINGS[0]
             )
